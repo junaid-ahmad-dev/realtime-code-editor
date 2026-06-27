fdb3e35ee6070f6c1aabd2e170cbf2c02b3384cb
junaid-ahmad-dev <junaidahmad0610@gmail.com>
2026-06-27 23:04:01 +0530

    Integrated socket

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
-import express from "express";
+import express from "express"
+import { createUser } from "http"
+import { Server } from "socket.io"
+import cors from "y-socket.io/dist/"
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res)=> {
+    res.send("Hello World!");
+});
+
+app.listen(3000,()=> {
+    console.log("Server is running on port 3000");
+});
